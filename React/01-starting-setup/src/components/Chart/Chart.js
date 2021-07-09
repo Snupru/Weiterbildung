7dff0660bf50b38f4ccbe84acdd0b2d04604ba93
Snupru <43205851+Snupru@users.noreply.github.com>
2021-07-09 13:21:59 +0200

    blub

---
+++ React/01-starting-setup/src/components/Chart/Chart.js
@@ -0,0 +1,21 @@
+import ChartBars from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMax = Math.max(...dataPointValues);
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBars
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMax}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
--- React/01-starting-setup/src/components/ExpenseItem.js
+++ React/01-starting-setup/src/components/ExpenseItem.js
@@ -1,27 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import ExpenseDate from './ExpenseDate';
-import Card from './Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
 
-function ExpenseItem(props){
-   const [title, setTitle] = useState(props.title);
-   
-    const clickHandler = () => {
-        setTitle('Updated!!!');
-        console.log('Clicked!!');
-    };
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
 
-    return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">{props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change title</button>
-        </Card>
-    );
+  const clickHandler = () => {
+    setTitle("Updated!!!");
+  };
+
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change title</button>
+      </Card>
+    </li>
+  );
 }
 
 export default ExpenseItem;
